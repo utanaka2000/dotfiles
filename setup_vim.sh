c2a0ea0883e3daca2c6713c1cb354b203d4611d1
Yu Tanaka <utanaka@A1007057utanakaComDW.local>
2023-10-13 19:05:16 +0900

    tweak

--- setup_vim.sh
+++ setup_vim.sh
@@ -3,3 +3,5 @@
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 mkdir -p $HOME/.vim/colors
 ln -s $HOME/dotfiles/.vim/colors/hybrid.vim $HOME/.vim/colors/hybrid.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+brew install deno
