488374f131e0fedb7689c4185d5b0ae1f5eea16d
utanaka2000 <27200337+utanaka2000@users.noreply.github.com>
2025-02-24 21:20:10 +0900

    feat(nvim): add lazy.nvim bootstrap and plugin configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,4 @@
+require("config.lazy")
 vim.keymap.set('n', '<C-j>', '<Esc>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-j>', '<Esc>', { noremap = true, silent = true })
-vim.opt.clipboard = "unnamedplus"  -- クリップボードの利用
-
-
-if vim.g.vscode then
-	return
-end
-
-
 
