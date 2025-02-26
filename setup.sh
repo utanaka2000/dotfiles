012b3b150eb880315b172ea0b9ae00f2ee9c8d85
utanaka2000 <27200337+utanaka2000@users.noreply.github.com>
2025-02-26 22:02:34 +0900

    fix: remove f option in ln commands

--- setup.sh
+++ setup.sh
@@ -18,12 +18,12 @@ mkdir -p $HOME/.zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions $HOME/.zsh/zsh-completions
-ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
+ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
 
 
 # vim
 mkdir -p $HOME/.vim
-ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
+ln -s $DOTFILES_DIR/vim/.vimrc $HOME/.vimrc
 curl -fLo $HOME/.vim/colors/hybrid.vim --create-dirs \
     https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
@@ -33,14 +33,14 @@ curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
 mkdir -p $HOME/.config
 
 # neovim
-ln -sf $DOTFILES_DIR/nvim $HOME/.config
+ln -s $DOTFILES_DIR/nvim $HOME/.config
 
 
 if [ "$OS_TYPE" = "Darwin" ]; then
     # karabiner
-    ln -sf $DOTFILES_DIR/karabiner $HOME/.config
+    ln -s $DOTFILES_DIR/karabiner $HOME/.config
 
     # vscode
-    ln -sf $DOTFILES_DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-    ln -sf $DOTFILES_DIR/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+    ln -s $DOTFILES_DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+    ln -s $DOTFILES_DIR/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
 fi
